dd2e1f5c5a96be74cada18a4ef60f93489e4bdb5
Yuri Pinheiro <yuripinheirot@gmail.com>
2024-05-05 12:55:37 -0400

    chore: init project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# unopar-extension-hack
 # fluency-academy-dark-mode
---
+++ content.js
@@ -0,0 +1,6 @@
+const applyDarkMode = () => {
+  document.querySelector('.post').style.backgroundColor = '#222'
+  console.log('Dark mode applied')
+}
+
+applyDarkMode()
